5b99596742d021081dc792f732587339b7c33b7d
moolnam <moolnam>
2022-01-17 12:56:48 +0900

    Initial Commit

---
+++ SwimmingTimer/SwimmingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimmingTimerApp.swift
+//  SwimmingTimer
+//
+//  Created by KimJongHee on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct SwimmingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
